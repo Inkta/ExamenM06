4ae763bf69266b7f1e4fceb036a554763a69b177
inkta <mquerluque@gmail.com>
2015-03-13 17:54:00 +0000

    hokuas

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('index');
